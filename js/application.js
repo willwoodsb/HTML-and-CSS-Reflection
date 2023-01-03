77910b7e0240fe768d7b88c5128cecdc5c5f3d5c
Will Woods Ballard <willwoodsb@gmail.com>
2023-01-03 11:17:18 +0000

    added slider functionality without content

---
+++ js/application.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items: 1,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 4000,
+        autoplayHoverPause: true,
+        dots: true
+    });
+  });
