89906b0b2e59999fb11c073cc6b0c950812fa4b7
Will Woods Ballard <willwoodsb@gmail.com>
2023-01-06 11:59:18 +0000

    fixed bugs highlighted in peer review

--- js/application.js
+++ js/application.js
@@ -69,66 +69,66 @@ $(window).scroll(function(){
 //cookie popup
 //---------------------
 
-//on document load (setTimeout added just to make it look smoother)
-$(document).ready(function(){
-  disableScroll();
-  setTimeout(function() {
-    // set cookies
-    var cookieName= "CookieConsent";
-    var cookieValue="Accepted";
-    var cookieExpireDays= 30;
+// //on document load (setTimeout added just to make it look smoother)
+// $(document).ready(function(){
+//   disableScroll();
+//   setTimeout(function() {
+//     // set cookies
+//     var cookieName= "CookieConsent";
+//     var cookieValue="Accepted";
+//     var cookieExpireDays= 30;
 
-    // when users click the accept button
-    let acceptCookie= document.getElementById("acceptCookie");
-    acceptCookie.onclick= function(){
-        createCookie(cookieName, cookieValue, cookieExpireDays);
-    }
-    // function to set cookie in web browser
-    let createCookie= function(cookieName, cookieValue, cookieExpireDays){
-      let currentDate = new Date();
-      currentDate.setTime(currentDate.getTime() + (cookieExpireDays*24*60*60*1000));
-      let expires = "expires=" + currentDate.toGMTString();
-      document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
-      if(document.cookie){
-        document.getElementById("cookiePopup-overlay").style.display = "none";
-        document.getElementById("cookiePopup").style.display = "none";
-        enableScroll();
-      }else{
-        alert("Unable to set cookie. Please allow all cookies site from cookie setting of your browser");
-      }
-    }
-    // retrieve specific cookie from web browser
-    let getCookie= function(cookieName){
-      let name = cookieName + "=";
-      let decodedCookie = decodeURIComponent(document.cookie);
-      let ca = decodedCookie.split(';');
-      for(let i = 0; i < ca.length; i++) {
-        let c = ca[i];
-        while (c.charAt(0) == ' ') {
-          c = c.substring(1);
-        }
-        if (c.indexOf(name) == 0) {
-          return c.substring(name.length, c.length);
-        }
-      }
-      return "";
-    }
-    // check to see if the cookie has been set or not (and therefore if should display consent box or not)
-    let checkCookie= function(){
-      let check=getCookie(cookieName);
-      if(check==""){
-          document.getElementById("cookiePopup-overlay").style.display = "block";
-          document.getElementById("cookiePopup").style.display = "block";
+//     // when users click the accept button
+//     let acceptCookie= document.getElementById("acceptCookie");
+//     acceptCookie.onclick= function(){
+//         createCookie(cookieName, cookieValue, cookieExpireDays);
+//     }
+//     // function to set cookie in web browser
+//     let createCookie= function(cookieName, cookieValue, cookieExpireDays){
+//       let currentDate = new Date();
+//       currentDate.setTime(currentDate.getTime() + (cookieExpireDays*24*60*60*1000));
+//       let expires = "expires=" + currentDate.toGMTString();
+//       document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+//       if(document.cookie){
+//         document.getElementById("cookiePopup-overlay").style.display = "none";
+//         document.getElementById("cookiePopup").style.display = "none";
+//         enableScroll();
+//       }else{
+//         alert("Unable to set cookie. Please allow all cookies site from cookie setting of your browser");
+//       }
+//     }
+//     // retrieve specific cookie from web browser
+//     let getCookie= function(cookieName){
+//       let name = cookieName + "=";
+//       let decodedCookie = decodeURIComponent(document.cookie);
+//       let ca = decodedCookie.split(';');
+//       for(let i = 0; i < ca.length; i++) {
+//         let c = ca[i];
+//         while (c.charAt(0) == ' ') {
+//           c = c.substring(1);
+//         }
+//         if (c.indexOf(name) == 0) {
+//           return c.substring(name.length, c.length);
+//         }
+//       }
+//       return "";
+//     }
+//     // check to see if the cookie has been set or not (and therefore if should display consent box or not)
+//     let checkCookie= function(){
+//       let check=getCookie(cookieName);
+//       if(check==""){
+//           document.getElementById("cookiePopup-overlay").style.display = "block";
+//           document.getElementById("cookiePopup").style.display = "block";
           
-      }else{
-          document.getElementById("cookiePopup-overlay").style.display = "none";
-          document.getElementById("cookiePopup").style.display = "none";
-          enableScroll();
-      }
-    }
-  checkCookie();
-  }, 300);
-});
+//       }else{
+//           document.getElementById("cookiePopup-overlay").style.display = "none";
+//           document.getElementById("cookiePopup").style.display = "none";
+//           enableScroll();
+//       }
+//     }
+//   checkCookie();
+//   }, 300);
+// });
 
 
 //functions to enable and disable scrolling when cookie popup opens and closes
@@ -153,8 +153,7 @@ const $contentToSlide = $('.content-wrapper, #hamburger-menu');
 const $hamMenu = $('#hamburger-menu');
 let scrollPos;
 
-$hamburger.click(function($e) {
-
+$('.hamburger, #grey-out').click(function($e) {
   //if the menu is hidden (i.e. not to the left) run this code
   if (left == false) {
     //set left to true and get the scroll position
@@ -168,7 +167,7 @@ $hamburger.click(function($e) {
       .addClass('noscroll');
     $('.content-wrapper').css('top', -scrollPos);
     $('#grey-out').css('top', scrollPos);
-    if (scrollPos>=600) {
+    if (stuck == true) {
       $header.css('top', scrollPos-300);
     }
     $header
@@ -206,7 +205,7 @@ $hamburger.click(function($e) {
         .css('width', '');
       left = false;
     }, 500);
-    console.log(scrollPos);
+
   }
 
 });
